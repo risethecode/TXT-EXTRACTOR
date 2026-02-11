549c3836dbf630559b284f2c2e07e5de74ff2eb5
risethecode <dayarmustfa2@gmail.com>
2026-02-11 22:16:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import os
+import threading
+import subprocess
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from spidy'
+@app.route("/")
+def home():
+    return "Bot is running 🚀"
 
+def run_bot():
+    subprocess.Popen(["python", "-m", "Extractor"])
 
 if __name__ == "__main__":
-    app.run()
-  
+    threading.Thread(target=run_bot).start()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
